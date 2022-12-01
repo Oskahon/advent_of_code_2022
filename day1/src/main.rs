43bae4900cee61cee85fcd47c396049471b04fdd
Oskari Ahonen <ahonenoskari@gmail.com>
2022-12-01 11:07:05 +0200

    day1 refactoring

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,38 +1,26 @@
-use std::fs;
-
 fn main() {
-    let data = fs::read_to_string("data.txt")
-        .expect("Error reading file");
+    let data = include_str!("../testi.txt");
 
-    let mut top1 = 0;
-    let mut top2 = 0;
-    let mut top3 = 0;
-    let mut carried = 0;
+    let mut calories = Vec::new();
 
-    for line in data.lines() {
-        if line.len() == 0 {
-            match carried {
-                n if carried > top1 => {
-                    top3 = top2;
-                    top2 = top1;
-                    top1 = n;
-                },
-                n if carried > top2 => {
-                    top3 = top2;
-                    top2 = n;
-                },
-                n if carried > top3 => top3 = n,
-                _ => ()
-            }
-            carried = 0;
-            continue;
-        }
-        let calories: i32 = line.parse().expect("Error parsing");
-        carried += calories;
+    let elves = data.split("\r\n\r\n").collect::<Vec<_>>();
+
+    for elf in elves {
+        let sum: i32 = elf
+            .lines()
+            .map(|n| n.parse::<i32>().unwrap())
+            .sum();
+
+        calories.push(sum);
     }
 
-    println!("part1: {}", top1);
 
-    let top_part2 = top1 + top2 + top3;
-    println!("part2: {}", top_part2);
+    calories.sort();
+    calories.reverse();
+
+    let top1 = calories.first().unwrap();
+    let top3: i32 = calories.iter().take(3).sum();
+
+    println!("part1: {top1}");
+    println!("part2: {top3}");
 }
